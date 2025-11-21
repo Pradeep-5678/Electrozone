1c42b9bfa35e881a274fc4a46757f3b08a578073
PRADEEP <pradeepbodduru5678@gmail.com>
2025-11-21 16:15:40 +0530

    Initial Commit

---
+++ OnlineElectronicStore/src/main/java/com/pack1/DBinfo.java
@@ -0,0 +1,9 @@
+package com.pack1;
+
+public interface DBinfo 
+{
+	public final static String driver="oracle.jdbc.OracleDriver";
+	public final static String dburl="jdbc:oracle:thin:@localhost:1521:orcl";
+	public final static String dbuname="MYDB";
+	public final static String dbpwd="1234";
+}
